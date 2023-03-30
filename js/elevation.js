2ee15d78dbe4b83fa66d7058ddbdba888f4a65c5
tkyk.niimura <tkyk.niimura@gmai.com>
2023-03-30 23:45:33 +0900

    fix chart

--- js/elevation.js
+++ js/elevation.js
@@ -237,3 +237,30 @@ export const getElevations = async (lat1, lng1, lat2, lng2) => {
   }
   return elevations;
 };
+
+/**
+ * calculate distance in kilometers between two points specified by degrees of latitude and longitude
+ *
+ * @author @kawanet
+ * @license MIT
+ * @see https://gist.github.com/kawanet/15c5a260ca3b98bd080bb87cdae57230
+ * @param {number} lat1 - degree of latitude of origin
+ * @param {number} lng1 - degree of longitude of origin
+ * @param {number} lat2 - degree of latitude of destination
+ * @param {number} lng2 - degree of longitude of destination
+ * @return {number} distance in kilometers between origin and destination
+ */
+
+export const getDistance = (lat1, lng1, lat2, lng2) => {
+  lat1 *= Math.PI / 180;
+  lng1 *= Math.PI / 180;
+  lat2 *= Math.PI / 180;
+  lng2 *= Math.PI / 180;
+  return (
+    6371 *
+    Math.acos(
+      Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
+        Math.sin(lat1) * Math.sin(lat2)
+    )
+  );
+};
